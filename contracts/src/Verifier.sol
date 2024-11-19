bcfa6aa1419edeb8378cee2a351aa00803765ae9
Aqrare <takumitakumi0310@gmail.com>
2024-11-19 17:25:07 +0900

    delete comment

--- contracts/src/Verifier.sol
+++ contracts/src/Verifier.sol
@@ -2,35 +2,25 @@
 pragma solidity ^0.8.19;
 
 contract Verifier {
-    // メッセージハッシュと署名から署名者のアドレスを回復する関数
     function verifySignature(bytes32 messageHash, bytes memory signature) public pure returns (address) {
-        // メッセージハッシュをEthereumの署名形式に変換
         bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
-
-        // 署名からr, s, vを分解
         bytes32 r;
         bytes32 s;
         uint8 v;
-
         assembly {
             r := mload(add(signature, 32))
             s := mload(add(signature, 64))
             v := byte(0, mload(add(signature, 96)))
         }
-
-        // 署名から署名者のアドレスを回復
         return ecrecover(ethSignedMessageHash, v, r, s);
     }
 
-    // 署名の検証を行う関数
     function verifyECDSA(bytes32 messageHash, bytes memory signature, address expectedSigner)
         external
         pure
         returns (bool)
     {
-        // 署名から署名者のアドレスを回復
         address recoveredSigner = verifySignature(messageHash, signature);
-        // 期待する署名者と一致するか確認
         return recoveredSigner == expectedSigner;
     }
 }
